875ae5c770a49fc8b6b8a51e4b368ab430e4ffd3
nx <nx.smtech@gmail.com>
2021-07-24 10:24:16 +0300

    Mask confirm password field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -113,7 +113,7 @@
 
                                 {{--                            TODO Testing - set #password-confirm type to password--}}
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="text" class="form-control"
+                                    <input id="password-confirm" type="password" class="form-control"
                                            name="password_confirmation" autocomplete="new-password">
                                 </div>
                             </div>
