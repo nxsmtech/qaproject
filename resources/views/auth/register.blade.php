4c96910edf7497616b334227fbfbad6934910392
nx <nx.smtech@gmail.com>
2021-10-05 09:50:32 +0300

    Add html required parameter for name attribute

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
                                 <div class="col-md-6">
                                     <input id="name" type="text"
                                            class="form-control @error('name') is-invalid @enderror" name="name"
-                                           value="{{ old('name') }}" autocomplete="name" autofocus>
+                                           value="{{ old('name') }}" autocomplete="name" autofocus required>
 
                                     @error('name')
                                     <span class="invalid-feedback" role="alert">
